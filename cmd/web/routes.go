2311b060b794a33364e43744d221cdc78e78e778
iBoBoTi <iroegbucalistus@gmail.com>
2022-02-24 22:22:22 +0100

    feat: separate the routes from the main

---
+++ cmd/web/routes.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func (app *application) Routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	return mux
+}
