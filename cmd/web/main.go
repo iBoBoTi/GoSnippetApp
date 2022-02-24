f7a8b34f4ddaa71f86f5e0f658a236787b73e17f
iBoBoTi <iroegbucalistus@gmail.com>
2022-02-24 23:35:36 +0100

    feat: add the mysql database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
 )
 
+func OpenDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dns)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dns := flag.String("dns", "root:Boboti40@/go_snippet?parseTime=true", "MySQL Connection string")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := OpenDB(*dns)
+	defer db.Close()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
 	app := &application{
 		errorLog: errorLog,
