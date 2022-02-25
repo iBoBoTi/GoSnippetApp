11f9d3f0aefe677bebd14bfa895c004013f02651
iBoBoTi <iroegbucalistus@gmail.com>
2022-02-25 14:45:50 +0100

    fix: get data from the mysql database using the necessary query

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -11,12 +11,30 @@ type SnippetModel struct {
 
 // Insert This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := "INSERT INTO snippets (title, content, created, expires) VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))"
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
 
 // Get This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	s := &models.Snippet{}
+	stmt := "SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?"
+	err := m.DB.QueryRow(stmt, id).Scan(&s.ID, &s.Content, &s.Title, &s.Created, &s.Expires)
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+	return s, nil
 }
 
 // Latest This will return the 10 most recently created snippets.
