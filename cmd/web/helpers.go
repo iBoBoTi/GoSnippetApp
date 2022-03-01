45a69c0aafa614cff39aad443b4d4cc2e77d01d0
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-01 01:29:58 +0100

    feat: add defaultdata helper function

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(rw http.ResponseWriter, err error) {
@@ -20,14 +22,26 @@ func (app *application) notFound(rw http.ResponseWriter) {
 	app.clientError(rw, http.StatusNotFound)
 }
 
-func (app *application) render(rw http.ResponseWriter, req http.Request, name string, td templateData) {
+func (app *application) render(rw http.ResponseWriter, req *http.Request, name string, td *templateData) {
 	ts, ok := app.templateCache[name]
 	if !ok {
 		app.serverError(rw, fmt.Errorf("The template %s oes not exist", name))
 		return
 	}
-	err := ts.Execute(rw, td)
+
+	buf := new(bytes.Buffer)
+
+	err := ts.Execute(buf, app.addDefaultData(td, req))
 	if err != nil {
 		app.serverError(rw, err)
 	}
+	buf.WriteTo(rw)
+}
+
+func (app *application) addDefaultData(td *templateData, req *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
 }
