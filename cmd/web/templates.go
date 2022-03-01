45115a1d9e5f966a76d7013d20f496f3451c96da
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-01 01:32:23 +0100

    feat: add custom template functions to set the date to human readable date

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,7 +3,16 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 )
+
+func humanDate(t time.Time) string {
+	return t.Format("01 Jan 2022 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
@@ -15,7 +24,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
