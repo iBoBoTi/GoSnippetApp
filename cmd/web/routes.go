90392b56214cd8df4b127c576c1a7fb6ed2052e6
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-01 22:19:01 +0100

    feat: setup alice package to chain the middlewares to the servemux

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,13 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) Routes() http.Handler {
+
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
