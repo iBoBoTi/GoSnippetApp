1860331e1cfad4faeb6037eb3c787d8eab44eabc
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-04 23:55:02 +0100

    feat: add routes for user login, logout, and signup

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,11 @@ func (app *application) Routes() http.Handler {
 	mux.Get("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippetForm))
 	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
 	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
+	mux.Get("/user/signup", app.signupUserForm)
+	mux.Post("/user/signup", app.signupUser)
+	mux.Get("/user/login", app.loginUserForm)
+	mux.Post("/user/login", app.loginUser)
+	mux.Post("/user/logout", app.logoutUser)
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 	return standardMiddleware.Then(mux)
 }
