cce48516cdeb4b575d2b43d0a387717695cecc36
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-09 00:11:40 +0100

    feat: add the insert user method to the user struct method

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -2,7 +2,11 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
+	"github.com/go-sql-driver/mysql"
 	"github.com/iBoBoTi/go-snippet/pkg/models"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
 )
 
 type UserModel struct {
@@ -10,6 +14,22 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+	stmt := `INSERT INTO users (name, email, hashed_password, created) VALUES(?, ?, ?, UTC_TIMESTAMP())`
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		var mySQLError *mysql.MySQLError
+		if errors.As(err, &mySQLError) {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message,
+				"users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 
