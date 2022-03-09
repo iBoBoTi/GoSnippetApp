a99f79f9aaf71b9be3831d66ecc0bf9637938c07
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-09 19:31:23 +0100

    feat: add the user authentication method to check for user details in form

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -34,7 +34,29 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPw []byte
+	stmt := `SELECT id, hashed_password FROM users WHERE email = ? AND active = TRUE`
+	row := m.DB.QueryRow(stmt, email)
+	err := row.Scan(&id, &hashedPw)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, models.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPw, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, models.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	return id, nil
 }
 
 func (m *UserModel) Get(id int) (*models.User, error) {
