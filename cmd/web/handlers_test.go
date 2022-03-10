e2e557df516d8a74bf63e7149b4318a78c629f14
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-10 23:30:06 +0100

    fix: implement the testutils functions on the TestPing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,58 +1,19 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
-//func TestPing(t *testing.T) {
-//	rr := httptest.NewRecorder()
-//
-//	r, err := http.NewRequest("GET", "/", nil)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	ping(rr, r)
-//	rs := rr.Result()
-//	if rs.StatusCode != http.StatusOK {
-//		t.Errorf("want %v, got %v", http.StatusOK, rs.StatusCode)
-//	}
-//
-//	defer rs.Body.Close()
-//	body, err := ioutil.ReadAll(rs.Body)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	if string(body) != "OK" {
-//		t.Errorf("want %v, got %v", "OK", string(body))
-//	}
-//}
-
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(ioutil.Discard, "", 0),
-		infoLog:  log.New(ioutil.Discard, "", 0),
-	}
-
-	ts := httptest.NewTLSServer(app.Routes())
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.Routes())
 	defer ts.Close()
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("want %v, got %v", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-	body, err := ioutil.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
+	code, _, body := ts.get(t, "/ping")
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
 	}
 	if string(body) != "OK" {
-		t.Errorf("want %v, got %v", "OK", string(body))
+		t.Errorf("want body to equal %q", "OK")
 	}
 }
