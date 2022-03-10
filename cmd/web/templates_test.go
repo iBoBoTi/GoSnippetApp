d9f3bc0d5fd8b2ee58e22ac6e1f1d6d693edb39d
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-10 20:12:37 +0100

    fix: modify the humanDate test to take multiple test cases using table test

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -6,9 +6,21 @@ import (
 )
 
 func Test_humanDate(t *testing.T) {
-	tm := time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC)
-	got := humanDate(tm)
-	if got != tm.Format(time.RFC1123) {
-		t.Errorf("want %q, got %q", tm.Format(time.RFC1123), got)
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{"UTC", time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC), "Thu, 17 Dec 2020 10:00:00 UTC"},
+		{"Empty", time.Time{}, ""},
+		{"CET", time.Date(2020, 12, 17, 10, 0, 0, 0, time.FixedZone("CET", 1*60*60)), "Thu, 17 Dec 2020 10:00:00 CET"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := humanDate(tt.tm)
+			if got != tt.want {
+				t.Errorf("want %q, got %q", tt.want, got)
+			}
+		})
 	}
 }
